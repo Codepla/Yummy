21b6691a21009141682bbc2474ee9e50a56075a8
5uw1st <west_blog@163.com>
2017-06-21 23:04:36 +0800

    encapsulate get,post method use jquery

--- Yummy/static/js/base/base.js
+++ Yummy/static/js/base/base.js
@@ -1,15 +1,21 @@
-function get(url){
+function ajaxGet(url){
     /*we can use it to get data from server*/
 
     if(!url || typeof url != "string"){
         console.log("URL格式不正确");
     }
+    var data_type = 'json';
     $.ajax({
         url,
-        dataType='json',
+        data_type,
         function(data,status){
             if(status="success"){
-                return data;
+                try{
+                    return JSON.parse(data);
+                }catch(e){
+                    console.debug("返回数据转为JSON失败");
+                    return null;
+                }
             }else{
                 console.log("请求失败"+status);
             }
@@ -17,10 +23,35 @@ function get(url){
     });
 }
 
-//TODO
-
-function post(url,data){
+function ajaxPost(url,data){
     /*we can use it to post data to server*/
-    //TODO
-    console.log("TEST");
+    if(!url || typeof url != "string"){
+        console.log("URL格式不正确");
+    }
+    var request_data = "";
+    try{
+        request_data = JSON.stringify(data);
+    }catch(e){
+        console.debug("将请求JSON数据转化为字符串失败");
+    }
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: request_data,
+        dataType: "json",
+        success: function(data){
+            try{
+                return JSON.parse(data);
+            }catch(e){
+                console.debug("返回数据转为JSON失败");
+                return null;
+            }
+        },
+        error: function(jqXHR){
+            alert("请求参数错误"+jqXHR.status);
+            return null;
+        }
+    })
 }
+
+//TODO
