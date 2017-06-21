082d990a41ea2ac80289835e974c0a25b552fd6f
sanhuo123 <673974521@qq.com>
2017-06-21 18:04:32 +0800

    Default

--- website/settings.py
+++ website/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'yummy',
+       'USER': 'root',
+       'PASSWORD': 'root',
+       'HOST': 'localhost',
+       'PORT': '3306'
     }
 }
 
